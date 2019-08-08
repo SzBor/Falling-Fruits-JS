50b47941a573c5884296982a0fb3943ff513e2d0
Szymon Borucki <szymonborucki@op.pl>
2019-08-08 05:19:58 +0200

    created nodes in order to add and remove color to cells later on

--- game.js
+++ game.js
@@ -2,4 +2,13 @@ const board = document.querySelector('#board');
 
 makeBoard(board, 10);
 
+document.querySelector('.cell').classList.add('fruit');
 
+
+const fruitNode =document.querySelector('.fruit')
+
+fruitNode.classList.remove("fruit")
+
+const targetFruitNode = document.querySelector('.row:nth-child(2) .cell')
+
+targetFruitNode.classList.add('fruit');
