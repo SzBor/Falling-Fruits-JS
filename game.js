e05489d688bfb64a4789dbca878fc6e71e1f98ad
Szymon Borucki <szymon@MacBook-Pro-Szymon.local>
2019-08-14 04:34:24 +0200

    adding .fruit class to a random cell in 1st row

--- game.js
+++ game.js
@@ -1,14 +1,34 @@
-const board = document.querySelector('#board');
+"use strict";
+
+const board = document.querySelector("#board");
 
 makeBoard(board, 10);
 
-document.querySelector('.cell').classList.add('fruit');
+setInterval(function() {
+  const allFruitNodes = document.querySelectorAll(".fruit");
 
+  allFruitNodes.forEach(function(element) {
+    element.classList.remove("fruit");
 
-const fruitNode =document.querySelector('.fruit')
+    const columnIndex = getIndexWithinParent(element);
+    const nextRow = getNextRow(element);
+    if (nextRow === null) {
+      alert("game over");
+      return;
+    }
+    const targetNode = nextRow.querySelector(
+      ".cell:nth-child(" + (columnIndex + 1) + ")"
+    );
+    targetNode.classList.add(".fruit");
+  });
+}, 1000);
 
-fruitNode.classList.remove("fruit")
-
-const targetFruitNode = document.querySelector('.row:nth-child(2) .cell')
-
-targetFruitNode.classList.add('fruit');
+setInterval(function(){
+    const allFreeCellsInFirstRow = document.querySelectorAll(
+        ".row:nth-child(1) .cell:not(.friut)"
+    )
+    const howManyFreeCells = allFreeCellsInFirstRow.length;
+    const randomIndex = Math.floor(Math.random() * howManyFreeCells);
+    allFreeCellsInFirstRow[randomIndex].classList.add('fruit');
+    
+}, 1000);
--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 function makeBoard(target, size) {
   for (let y = 0; y < size; y += 1) {
-    let rowNode = createNode('row');
+    let rowNode = createNode("row");
 
     for (let x = 0; x < size; x += 1) {
-      let cellNode = createNode('cell');
+      let cellNode = createNode("cell");
 
       rowNode.appendChild(cellNode);
     }
@@ -12,8 +12,16 @@ function makeBoard(target, size) {
 }
 
 function createNode(className) {
-  const node = document.createElement('div');
+  const node = document.createElement("div");
   node.classList.add(className);
 
   return node;
+}
+
+function getIndexWithinParent(element) {
+  return Array.from(element.parentNode.children).indexOf(element);
+}
+
+function getNextRow(element) {
+  return element.parentElement.nextElementSibling;
 }
