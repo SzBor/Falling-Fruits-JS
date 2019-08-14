08eff31f8536cf063bdaf247886ecf37a6df536f
Szymon Borucki <szymon@MacBook-Pro-Szymon.local>
2019-08-14 06:08:12 +0200

    added basket and eventlisteners so that it moves

--- game.js
+++ game.js
@@ -4,10 +4,34 @@ const board = document.querySelector("#board");
 
 makeBoard(board, 10);
 
+const basketNode = document.querySelector(".row:last-child .cell");
+basketNode.classList.add("basket");
+
+window.addEventListener("keyup", function(event) {
+  const basketNode = document.querySelector(".basket");
+
+  if (event.code === "ArrowRight") {
+    const targetNode = basketNode.nextElementSibling;
+    if (targetNode === null) {
+      return;
+    }
+    basketNode.classList.remove("basket");
+    targetNode.classList.add("basket");
+  }
+  if (event.code === "ArrowLeft") {
+    const targetNode = basketNode.previousElementSibling;
+    if (targetNode === null) {
+      return;
+    }
+    basketNode.classList.remove("basket");
+    targetNode.classList.add("basket");
+  }
+});
+
 setInterval(function() {
   const allFruitNodes = document.querySelectorAll(".fruit");
-    allFruitNodes.forEach(function(element) {
-        element.classList.remove("fruit");
+  allFruitNodes.forEach(function(element) {
+    element.classList.remove("fruit");
 
     const columnIndex = getIndexWithinParent(element);
     const nextRow = getNextRow(element);
@@ -22,12 +46,11 @@ setInterval(function() {
   });
 }, 1000);
 
-setInterval(function(){
-    const allFreeCellsInFirstRow = document.querySelectorAll(
-        ".row:nth-child(1) .cell:not(.friut)"
-    )
-    const howManyFreeCells = allFreeCellsInFirstRow.length;
-    const randomIndex = Math.floor(Math.random() * howManyFreeCells);
-    allFreeCellsInFirstRow[randomIndex].classList.add('fruit');
-    
-}, 1000);
+setInterval(function() {
+  const allFreeCellsInFirstRow = document.querySelectorAll(
+    ".row:nth-child(1) .cell:not(.friut)"
+  );
+  const howManyFreeCells = allFreeCellsInFirstRow.length;
+  const randomIndex = Math.floor(Math.random() * howManyFreeCells);
+  allFreeCellsInFirstRow[randomIndex].classList.add("fruit");
+}, 3000);
